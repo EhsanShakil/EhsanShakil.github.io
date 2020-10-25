5cc76bd0ffa0c1900bc6cc26e7b66d6a924985c2
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-25 22:56:47 +0500

    Updating

--- src/App.js
+++ src/App.js
@@ -2,15 +2,40 @@ import React, { useState } from "react";
 import Home from "./components/Home";
 import Stacks from "./components/Stacks";
 import Projects from "./components/Projects";
+import { ImHome, ImStack } from "react-icons/im";
+import { MdWork } from "react-icons/md";
+import { MdPictureAsPdf } from "react-icons/md";
 
+import "./App.css";
 function App() {
   const [screen, setScreen] = useState(Home);
   return (
-    <div>
-      <h1>{screen}</h1>
-      <button onClick={() => setScreen(Home)}>Home</button>
-      <button onClick={() => setScreen(Stacks)}>Stacks</button>
-      <button onClick={() => setScreen(Projects)}>Projects</button>
+    <div className="container">
+      <div className="icons">
+        <button className="icon" title="Home" onClick={() => setScreen(Home)}>
+          <ImHome size="35" />
+        </button>
+        <button
+          className="icon"
+          title="Tech Stacks"
+          onClick={() => setScreen(Stacks)}
+        >
+          <ImStack size="35" />
+        </button>
+        <button
+          className="icon"
+          title="Projects"
+          onClick={() => setScreen(Projects)}
+        >
+          <MdWork size="35" />
+        </button>
+        <button className="icon" title="Resume" onClick={() => setScreen(Home)}>
+          <MdPictureAsPdf size="35" />
+        </button>
+      </div>
+      <div className="screen">
+        <span>{screen}</span>
+      </div>
     </div>
   );
 }
