e78f0d3eeef41ddd0f54e7913d50c89ef3f4a0f7
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-25 21:25:23 +0500

    Updating

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import Home from "./components/Home";
 import Stacks from "./components/Stacks";
 import Projects from "./components/Projects";
 
 function App() {
+  const [screen, setScreen] = useState(Home);
   return (
     <div>
-      <Home />
-      <Stacks />
-      <Projects />
+      <h1>{screen}</h1>
+      <button onClick={() => setScreen(Home)}>Home</button>
+      <button onClick={() => setScreen(Stacks)}>Stacks</button>
+      <button onClick={() => setScreen(Projects)}>Projects</button>
     </div>
   );
 }
