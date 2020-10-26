77f1c1778b017e2f7e50d6cbb50b8e63083ddfec
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-26 23:05:28 +0500

    Updating

--- src/components/Stacks.js
+++ src/components/Stacks.js
@@ -2,8 +2,203 @@ import React from "react";
 
 const Stacks = () => {
   return (
-    <div>
-      <h1>Stacks Component</h1>
+    <div className="stacks">
+      <h1>Tech Stacks</h1>
+      <a href="https://www.w3.org/html/" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/html5/html5-original-wordmark.svg"
+          alt="html5"
+          width="60"
+          height="60"
+          title="HTML5"
+        />
+      </a>
+      <a href="https://www.w3schools.com/css/" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/css3/css3-original-wordmark.svg"
+          alt="css3"
+          width="60"
+          height="60"
+          title="CSS3"
+        />
+      </a>
+      <a href="https://getbootstrap.com" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/bootstrap/bootstrap-plain.svg"
+          alt="bootstrap"
+          width="60"
+          height="60"
+          title="BOOTSTRAP"
+        />
+      </a>
+      <a
+        href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
+        target="_blank"
+      >
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/javascript/javascript-original.svg"
+          alt="javascript"
+          width="60"
+          height="60"
+          title="JAVASCRIPT"
+        />
+      </a>
+      <a href="https://reactjs.org/" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/react/react-original-wordmark.svg"
+          alt="react"
+          width="60"
+          height="60"
+          title="REACTJS"
+        />
+      </a>
+      <a href="https://www.typescriptlang.org/" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/typescript/typescript-original.svg"
+          alt="typescript"
+          width="60"
+          height="60"
+          title="TYPESCRIPT"
+        />
+      </a>
+      <a href="https://reactnative.dev/" target="_blank">
+        <img
+          src="https://reactnative.dev/img/header_logo.svg"
+          alt="reactnative"
+          width="60"
+          height="60"
+          title="REACT-NATIVE"
+        />
+      </a>
+      <a href="https://redux.js.org" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/redux/redux-original.svg"
+          alt="redux"
+          width="60"
+          height="60"
+          title="REDUX"
+        />
+      </a>
+      <a href="https://nodejs.org" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/nodejs/nodejs-original-wordmark.svg"
+          alt="nodejs"
+          width="60"
+          height="60"
+          title="NODEJS"
+        />
+      </a>
+      <a href="https://expressjs.com" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/express/express-original-wordmark.svg"
+          alt="express"
+          width="60"
+          height="60"
+          title="EXPRESS"
+        />
+      </a>
+      <a href="https://graphql.org" target="_blank">
+        <img
+          src="https://www.vectorlogo.zone/logos/graphql/graphql-icon.svg"
+          alt="graphql"
+          width="60"
+          height="60"
+          title="GRAPHQL"
+        />
+      </a>
+      <a href="https://www.mongodb.com/" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/mongodb/mongodb-original-wordmark.svg"
+          alt="mongodb"
+          width="60"
+          height="60"
+          title="MONGODB"
+        />
+      </a>
+      <a href="https://firebase.google.com/" target="_blank">
+        <img
+          src="https://www.vectorlogo.zone/logos/firebase/firebase-icon.svg"
+          alt="firebase"
+          width="60"
+          height="60"
+          title="FIREBASE"
+        />
+      </a>
+      <a href="https://jestjs.io" target="_blank">
+        <img
+          src="https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg"
+          alt="jest"
+          width="60"
+          height="60"
+          title="JEST"
+        />
+      </a>
+      <a href="https://enzymejs.github.io/enzyme/" target="_blank">
+        <img
+          src="https://asset.jarombek.com/logos/enzyme.png"
+          alt="jest"
+          width="60"
+          height="60"
+          title="ENZYME"
+        />
+      </a>
+      <a href="https://postman.com" target="_blank">
+        <img
+          src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
+          alt="postman"
+          width="60"
+          height="60"
+          title="POSTMAN"
+        />
+      </a>
+      <a href="https://canvasjs.com" target="_blank">
+        <img
+          src="https://raw.githubusercontent.com/Hardik0307/Hardik0307/master/assets/canvasjs-charts.svg"
+          alt="canvasjs"
+          width="60"
+          height="60"
+          title="CANVASJS"
+        />
+      </a>
+      <a href="https://www.chartjs.org" target="_blank">
+        <img
+          src="https://www.chartjs.org/media/logo-title.svg"
+          alt="chartjs"
+          width="60"
+          height="60"
+          title="CHARTJS"
+        />
+      </a>
+      <a href="https://www.linux.org/" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/linux/linux-original.svg"
+          alt="linux"
+          width="60"
+          height="60"
+          title="LINUX"
+        />
+      </a>
+      <a href="https://www.photoshop.com/en" target="_blank">
+        <img
+          src="https://devicons.github.io/devicon/devicon.git/icons/photoshop/photoshop-plain.svg"
+          alt="photoshop"
+          width="60"
+          height="60"
+          title="ADOBE-PHOTOSHOP"
+        />
+      </a>
+      <a
+        href="https://www.adobe.com/in/products/illustrator.html"
+        target="_blank"
+      >
+        <img
+          src="https://www.vectorlogo.zone/logos/adobe_illustrator/adobe_illustrator-icon.svg"
+          alt="illustrator"
+          width="60"
+          height="60"
+          title="ADOBE-ILLUSTRATOR"
+        />
+      </a>
     </div>
   );
 };
