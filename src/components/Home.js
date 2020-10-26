2f089b56ac5d9d051ac32a953796f318a03bc84b
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-26 22:26:06 +0500

    Updating

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,17 @@
 import React from "react";
+import "../App.css";
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home Component</h1>
+    <div className="home">
+      <h1>JAVASCRIPT</h1>
+      <p>MERN Stack Developer</p>
+      <p>
+        Hello, I am Ahsan Shakeel, I am a Industrial & Manufacturing Engineer &
+        I switched my career in this field because of passion. I recently
+        completed my "MERN Stack Development" course and I have some good grips
+        on frontend technologies.
+      </p>
     </div>
   );
 };
