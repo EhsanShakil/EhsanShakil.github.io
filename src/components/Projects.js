13d3701568a6aa8cea05b4c33184aa302a5f3ed5
EhsanShakil <ahsanshakeel1994@gmail.com>
2020-10-27 13:03:35 +0500

    Updating

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,18 +2,39 @@ import React, { useState } from "react";
 import { AiFillCaretLeft, AiFillCaretRight } from "react-icons/ai";
 
 const Projects = () => {
+  let [projects, setProjects] = useState([
+    {
+      id: 1,
+      project_name: "COVID19-TRACKER",
+      url: "http://covid_19_tracker.surge.sh/",
+    },
+    {
+      id: 2,
+      project_name: "QUIZ-APP",
+      url: "https://quiz-app-project.netlify.app/",
+    },
+  ]);
   let [number, setNumber] = useState(1);
   return (
     <div className="projects">
       <h1>Projects</h1>
-      <p>{number}</p>
-      <button onClick={() => setNumber(++number)}>Increase</button>
-      <button onClick={() => setNumber(--number)}>
-        <AiFillCaretLeft />
-      </button>
-      <button onClick={() => setNumber(++number)}>
-        <AiFillCaretRight />
-      </button>
+      <div className="project">
+        <button className="arrow" onClick={() => setNumber(--number)}>
+          <AiFillCaretLeft size="30" />
+        </button>
+        {projects
+          .filter((project) => project.id === number)
+          .map((project, index) => (
+            <div key={index}>
+              <p>{project.id}</p>
+              <p>{project.project_name}</p>
+              <p>{project.url}</p>
+            </div>
+          ))}
+        <button className="arrow" onClick={() => setNumber(++number)}>
+          <AiFillCaretRight size="30" />
+        </button>
+      </div>
     </div>
   );
 };
